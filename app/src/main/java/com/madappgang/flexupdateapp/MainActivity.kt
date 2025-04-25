a8d4eb62543b17c9d46fd19400c759faed690f52
Eugene Prytula <ep@madappgang.com>
2025-04-25 17:28:43 +0300

    Update the sample app

--- app/src/main/java/com/madappgang/flexupdateapp/MainActivity.kt
+++ app/src/main/java/com/madappgang/flexupdateapp/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.madappgang.flexupdate.core.FlexUpdateManager
 import com.madappgang.flexupdate.core.types.UpdatePriority.MEDIUM
 import com.madappgang.flexupdate.core.types.UpdateStrategy.Manual
+import com.madappgang.flexupdateapp.BuildConfig.VERSION_CODE
+import com.madappgang.flexupdateapp.BuildConfig.VERSION_NAME
 import com.madappgang.flexupdateapp.ui.theme.FlexUpdateTheme
 
 
@@ -31,28 +33,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             FlexUpdateTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "App version",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        Text(
+                            modifier = Modifier.align(Alignment.Center),
+                            text = "Version code ${VERSION_CODE}\nVersion name:$VERSION_NAME"
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlexUpdateTheme {
-        Greeting("Android")
     }
 }
