032fdcd9bcab733b9eb11e2c42afb255bd1f43ed
Eugene Prytula <ep@madappgang.com>
2025-04-25 10:51:34 +0300

    Add code generation for lib

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.31.0"
 }
 
+val libVersion = "0.1.1"
+
 android {
     namespace = "com.madappgang.flexupdate.core"
     compileSdk = 35
@@ -15,8 +17,8 @@ android {
         applicationId = "com.madappgang.flexupdate.core"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = generateVersionCode(libVersion)
+        versionName = libVersion
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -53,17 +55,38 @@ android {
 publishing {
     publications {
         create<MavenPublication>("release") {
-            groupId = "com.madappgang.flexupdate"
-            artifactId = "core"
-            version = "0.1"
+            groupId = "io.github.evgeneprytula"
+            artifactId = "flex-update"
+            version = libVersion
 
             afterEvaluate {
                 from(components["release"])
             }
 
             pom {
+                name.set("FlexUpdate")
+                description.set("A library to manage the In-app update integration in Android")
+                url.set("https://github.com/EvgenePrytula/flex-update")
+
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("eugeneprytula")
+                        name.set("Eugene Prytula")
+                        email.set("ep@madappgang.com")
+                    }
+                }
+
                 scm {
-                    connection.set("scm:git:git://github.com/")
+                    connection.set("scm:git:git://github.com/EvgenePrytula/flex-update")
+                    developerConnection.set("scm:git:ssh://github.com/EvgenePrytula/flex-update")
+                    url.set("http://github.com/EvgenePrytula/flex-update")
                 }
             }
         }
@@ -97,4 +120,14 @@ dependencies {
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+fun generateVersionCode(versionName: String): Int {
+    val parts = versionName.split(".").map { it.toIntOrNull() ?: 0 }
+
+    val major = parts.getOrElse(0) { 0 }
+    val minor = parts.getOrElse(1) { 0 }
+    val patch = parts.getOrElse(2) { 0 }
+
+    return major * 10000 + minor * 100 + patch
 }
