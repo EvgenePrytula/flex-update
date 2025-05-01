6c2672cc8cd6a1d6f232674e7c53213d171d3d05
Eugene Prytula <ep@madappgang.com>
2025-05-01 15:11:13 +0300

    Reuse code for getting info

--- core/src/main/java/com/madappgang/flexupdate/core/FlexUpdateManager.kt
+++ core/src/main/java/com/madappgang/flexupdate/core/FlexUpdateManager.kt
@@ -4,17 +4,25 @@ import android.app.Activity.RESULT_OK
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts.StartIntentSenderForResult
+import com.google.android.gms.tasks.Tasks
+import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.install.model.AppUpdateType.FLEXIBLE
 import com.google.android.play.core.install.model.AppUpdateType.IMMEDIATE
+import com.google.android.play.core.install.model.UpdateAvailability.UPDATE_AVAILABLE
 import com.madappgang.flexupdate.core.handlers.FlexibleUpdateHandler
 import com.madappgang.flexupdate.core.handlers.ImmediateUpdateHandler
+import com.madappgang.flexupdate.core.types.UpdateInfoResult
+import com.madappgang.flexupdate.core.types.UpdatePriority
 import com.madappgang.flexupdate.core.types.UpdatePriority.CRITICAL
 import com.madappgang.flexupdate.core.types.UpdateStrategy
 import com.madappgang.flexupdate.core.types.UpdateStrategy.Auto
 import com.madappgang.flexupdate.core.types.UpdateStrategy.Manual
+import com.madappgang.flexupdate.core.types.getUpdateType
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
 
 
 /**
@@ -56,7 +64,7 @@ class FlexUpdateManager private constructor(
 
     suspend fun checkForUpdate() = withContext(Dispatchers.IO) {
         runCatching {
-            val updateInfoResult = updateStrategy.resolve(appUpdateManager)
+            val updateInfoResult = appUpdateManager.getUpdateInfoResult(updateStrategy)
             val updateHandler = when (val updateType = updateInfoResult?.updateType) {
                 IMMEDIATE -> withContext(Dispatchers.Main) {
                     ImmediateUpdateHandler(
@@ -77,6 +85,45 @@ class FlexUpdateManager private constructor(
                 else -> throw IllegalStateException("Unknown update type: $updateType")
             }
             updateHandler.startUpdateFlow(updateInfoResult.appUpdateInfo)
+        }
+    }
+}
+
+private suspend fun AppUpdateManager.getUpdateInfoResult(strategy: UpdateStrategy): UpdateInfoResult? {
+    return suspendCancellableCoroutine { continuation ->
+        try {
+            val info = Tasks.await(appUpdateInfo)
+
+            val updateType = when (strategy) {
+                Auto -> {
+                    val priority = UpdatePriority.fromPriority(info.updatePriority())
+                    priority.getUpdateType()
+                }
+
+                is Manual -> strategy.updatePriority.getUpdateType()
+            }
+
+
+            if (updateType == null) {
+                continuation.resume(null)
+                return@suspendCancellableCoroutine
+            }
+
+            val isUpdateAvailable = info.updateAvailability() == UPDATE_AVAILABLE
+
+            if (isUpdateAvailable) {
+                continuation.resume(
+                    UpdateInfoResult(
+                        updateType = updateType,
+                        appUpdateInfo = info
+                    )
+                )
+            } else {
+                continuation.resume(null)
+            }
+
+        } catch (e: Exception) {
+            continuation.resume(null)
         }
     }
 }
--- core/src/main/java/com/madappgang/flexupdate/core/types/UpdateStrategy.kt
+++ core/src/main/java/com/madappgang/flexupdate/core/types/UpdateStrategy.kt
@@ -1,12 +1,5 @@
 package com.madappgang.flexupdate.core.types
 
-import android.util.Log
-import com.google.android.gms.tasks.Tasks
-import com.google.android.play.core.appupdate.AppUpdateManager
-import com.google.android.play.core.install.model.UpdateAvailability.UPDATE_AVAILABLE
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlin.coroutines.resume
-
 
 /**
  * Created by Eugene Prytula on 25.04.2025.
@@ -15,72 +8,7 @@ import kotlin.coroutines.resume
 sealed class UpdateStrategy {
     data class Manual(
         val updatePriority: UpdatePriority
-    ) : UpdateStrategy() {
-        override suspend fun resolve(appUpdateManager: AppUpdateManager): UpdateInfoResult? {
-            return suspendCancellableCoroutine { continuation ->
-                val updateType = updatePriority.getUpdateType()
+    ) : UpdateStrategy()
 
-                if (updateType == null) {
-                    continuation.resume(null)
-                    return@suspendCancellableCoroutine
-                }
-
-                try {
-                    val info = Tasks.await(appUpdateManager.appUpdateInfo)
-
-                    val isUpdateAvailable = info.updateAvailability() == UPDATE_AVAILABLE
-
-                    if (isUpdateAvailable) {
-                        continuation.resume(
-                            UpdateInfoResult(
-                                updateType = updateType,
-                                appUpdateInfo = info
-                            )
-                        )
-                    } else {
-                        continuation.resume(null)
-                    }
-
-                } catch (e: Exception) {
-                    continuation.resume(null)
-                }
-            }
-        }
-    }
-
-    data object Auto : UpdateStrategy() {
-        override suspend fun resolve(appUpdateManager: AppUpdateManager): UpdateInfoResult? {
-            return suspendCancellableCoroutine { continuation ->
-                try {
-                    val info = Tasks.await(appUpdateManager.appUpdateInfo)
-
-                    val priority = UpdatePriority.fromPriority(info.updatePriority())
-                    val updateType = priority.getUpdateType()
-
-                    if (updateType == null) {
-                        continuation.resume(null)
-                        return@suspendCancellableCoroutine
-                    }
-
-                    val isUpdateAvailable = info.updateAvailability() == UPDATE_AVAILABLE
-
-                    if (isUpdateAvailable) {
-                        continuation.resume(
-                            UpdateInfoResult(
-                                updateType = updateType,
-                                appUpdateInfo = info
-                            )
-                        )
-                    } else {
-                        continuation.resume(null)
-                    }
-
-                } catch (e: Exception) {
-                    continuation.resume(null)
-                }
-            }
-        }
-    }
-
-    abstract suspend fun resolve(appUpdateManager: AppUpdateManager): UpdateInfoResult?
+    data object Auto : UpdateStrategy()
 }
